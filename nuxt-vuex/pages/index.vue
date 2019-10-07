004dcec210ded99c07f791ed4468c5c1cf9c0c24
macotok <monochromer@r8.ucom.ne.jp>
2019-10-07 17:32:06 +0900

    vuexについてクラシックモード、モジュールモードに分けて学習

---
+++ nuxt-vuex/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <!-- クラシックモードで実装 -->
+      <!--<p>{{ $store.state.message }}</p>-->
+      <!-- <button v-on:click="$store.commit('updateMessage')">update</button> -->
+      <!--<button v-on:click="$store.commit('updateMessage', 'Commit with payload')">update</button>-->
+      <!--<button v-on:click="$store.dispatch('updateMessageAction')">Dispatch</button>-->
+      <!--<button v-on:click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">Dispatch</button>-->
+
+      <!-- モジュールモードで実装 -->
+      <p>{{ $store.state.hello.message }}</p>
+      <button v-on:click="$store.dispatch('hello/updateMessageAction', 'Commit with module mode')">module mode</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
